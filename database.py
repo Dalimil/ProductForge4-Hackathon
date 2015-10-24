d2e9c18a300ceceee9882caed7a12a0e98d75f18
Dalimil <dalimilhajek@gmail.com>
2015-10-24 22:44:06 +0100

    just design

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ if('OPENSHIFT_DATA_DIR' in os.environ):
 if not os.path.isfile(sqlite_file):
 	conn = sqlite3.connect(sqlite_file)
 	c = conn.cursor()
-	c.execute("CREATE TABLE Events ( ID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(255), Location VARCHAR(255), City VARCHAR(255), Description VARCHAR(255), Organiser VARCHAR(255), Time INTEGER );");
+	c.execute("CREATE TABLE Events ( ID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(255), Location VARCHAR(255), City VARCHAR(255), Description VARCHAR(255), Organiser VARCHAR(255), Time VARCHAR(255) );");
 	c.execute("CREATE TABLE Interests ( ID INTEGER PRIMARY KEY AUTOINCREMENT, InterestID VARCHAR(255), EventID VARCHAR(255));")
 	conn.commit()
 	conn.close()
@@ -44,7 +44,7 @@ def getEventsWithId(id):
 def addEvent(data):
 	conn = sqlite3.connect(sqlite_file)
 	c = conn.cursor()
-	c.execute('INSERT INTO Events (Name, Location, City, Description, Organiser, Time) VALUES ("{}", "{}", "{}", "{}", "{}", {})'.format(data["name"], data["location"], data["city"], data["description"], data["organiser"], data["time"]))
+	c.execute('INSERT INTO Events (Name, Location, City, Description, Organiser, Time) VALUES ("{}", "{}", "{}", "{}", "{}", "{}")'.format(data["name"], data["location"], data["city"], data["description"], data["organiser"], data["time"]))
 	for tag in data["interests"]:
 		c.execute('INSERT INTO Interests (InterestID, EventID) VALUES ("{}", "{}")'.format(tag, data["name"]));
 	conn.commit()
